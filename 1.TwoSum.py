ff704397cb17963513ebe58fe116e66f07f7829d
Z-421 <khazaeireza97@gmail.com>
2024-12-05 13:09:13 +0100

    fixing the len()

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-1):
+        for i in range(len(nums)):
+            for j in range(len(nums)):
                 if(i == j):
                     continue
                 else:
